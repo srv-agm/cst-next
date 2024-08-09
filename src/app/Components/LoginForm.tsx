39dc02c207afc475daaf83fb55fa60b068ded37b
Agam <agam.srivastava@mfilterit.com>
2024-08-09 18:06:12 +0530

    commit

--- src/app/Components/LoginForm.tsx
+++ src/app/Components/LoginForm.tsx
@@ -1,67 +1,67 @@
-"use client";
-import {
-  loginAction,
-  LoginFormState,
-  ServerError,
-  validationError,
-} from "./actions";
-import React from "react";
-import { Button, Form, Spinner } from "react-bootstrap";
-import { useFormState, useFormStatus } from "react-dom";
+// "use client";
+// import {
+//   loginAction,
+//   LoginFormState,
+//   ServerError,
+//   validationError,
+// } from "./actions";
+// import React from "react";
+// import { Button, Form, Spinner } from "react-bootstrap";
+// import { useFormState, useFormStatus } from "react-dom";
 
-export default function LoginForm() {
-  //@ts-ignore
-  const [state, action] = useFormState<LoginFormState>(loginAction, {
-    username: "",
-    password: "",
-    message: "",
-  });
-  return (
-    <Form action={action} className="d-flex flex-column">
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="john.doe@mail.com"
-          name="username"
-        />
-        {(state?.errors as validationError)?.username && (
-          <p className="text-danger text-sm">
-            {(state?.errors as validationError)?.username}
-          </p>
-        )}
-      </Form.Group>
+// export default function LoginForm() {
+//   //@ts-ignore
+//   const [state, action] = useFormState<LoginFormState>(loginAction, {
+//     username: "",
+//     password: "",
+//     message: "",
+//   });
+//   return (
+//     <Form action={action} className="d-flex flex-column">
+//       <Form.Group controlId="formBasicEmail">
+//         <Form.Label>Email</Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="john.doe@mail.com"
+//           name="username"
+//         />
+//         {(state?.errors as validationError)?.username && (
+//           <p className="text-danger text-sm">
+//             {(state?.errors as validationError)?.username}
+//           </p>
+//         )}
+//       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
-        {(state?.errors as validationError)?.password && (
-          <p className="text-danger text-sm">
-            {(state?.errors as validationError)?.password}
-          </p>
-        )}
-      </Form.Group>
-      {(state?.errors as ServerError)?.message && (
-        <p className="text-danger text-sm">
-          {(state?.errors as ServerError)?.message}
-        </p>
-      )}
-      <LoginButton />
-    </Form>
-  );
-}
+//       <Form.Group controlId="formBasicPassword">
+//         <Form.Label>Password</Form.Label>
+//         <Form.Control type="password" name="password" placeholder="Password" />
+//         {(state?.errors as validationError)?.password && (
+//           <p className="text-danger text-sm">
+//             {(state?.errors as validationError)?.password}
+//           </p>
+//         )}
+//       </Form.Group>
+//       {(state?.errors as ServerError)?.message && (
+//         <p className="text-danger text-sm">
+//           {(state?.errors as ServerError)?.message}
+//         </p>
+//       )}
+//       <LoginButton />
+//     </Form>
+//   );
+// }
 
-function LoginButton() {
-  const { pending } = useFormStatus();
-  return (
-    <Button
-      className="mt-2 text-center"
-      variant="primary"
-      type="submit"
-      disabled={pending}
-    >
-      {!pending && "Login"}
-      {pending && <Spinner size="sm" />}
-    </Button>
-  );
-}
+// function LoginButton() {
+//   const { pending } = useFormStatus();
+//   return (
+//     <Button
+//       className="mt-2 text-center"
+//       variant="primary"
+//       type="submit"
+//       disabled={pending}
+//     >
+//       {!pending && "Login"}
+//       {pending && <Spinner size="sm" />}
+//     </Button>
+//   );
+// }
--- src/app/Components/actions.ts
+++ src/app/Components/actions.ts
@@ -1,72 +1,72 @@
-'use server';
+// 'use server';
 
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { z } from 'zod';
+// import { cookies } from 'next/headers';
+// import { redirect } from 'next/navigation';
+// import { z } from 'zod';
 
-export type validationError = {
-  username?: string[] | undefined;
-  password?: string[] | undefined;
-};
+// export type validationError = {
+//   username?: string[] | undefined;
+//   password?: string[] | undefined;
+// };
 
-export type ServerError = { message?: any };
+// export type ServerError = { message?: any };
 
-export type LoginFormState = {
-  errors: validationError | ServerError;
-};
+// export type LoginFormState = {
+//   errors: validationError | ServerError;
+// };
 
-const schema = z.object({
-  username: z
-    .string({
-      required_error: 'Email is required',
-      invalid_type_error: 'Invalid Email',
-    })
-    .email('Invalid format'),
-  password: z
-    .string({
-      required_error: 'Password is required',
-      invalid_type_error: 'Invalid Password',
-    })
-    .min(8, 'Too short!'),
-});
+// const schema = z.object({
+//   username: z
+//     .string({
+//       required_error: 'Email is required',
+//       invalid_type_error: 'Invalid Email',
+//     })
+//     .email('Invalid format'),
+//   password: z
+//     .string({
+//       required_error: 'Password is required',
+//       invalid_type_error: 'Invalid Password',
+//     })
+//     .min(8, 'Too short!'),
+// });
 
-export async function loginAction(
-  _: LoginFormState,
-  formData: FormData
-): Promise<LoginFormState> {
-  const username = formData.get('username') + '';
-  const password = formData.get('password') + '';
-  const validatedFields = schema.safeParse({ username, password });
-  if (!validatedFields.success) {
-    return { errors: validatedFields.error.flatten().fieldErrors };
-  }
-  console.log('LOGIN:', username);
-  const url = process.env.BASE_API_DOMAIN + '/login';
-  let token, route, pkg;
-  try {
-    const USER = await fetch(url, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({ username, password }),
-    }).then(async r => {
-      if ([200, 400].includes(r.status)) return r.json();
-      else throw new Error('Server Error');
-    });
-    if (!USER.token) return { errors: { message: USER.message } };
-    cookies().set('token', USER.token, {
-      httpOnly: true,
-      secure: true,
-      expires: 10700,
-      maxAge: 10700,
-    });
-    token = USER.token;
-    route = USER.menu_route;
-    pkg = USER.package.package_name;
-  } catch (error) {
-    console.log(error);
-    return { errors: { message: 'please try after sometime' } };
-  }
-  const p = new URLSearchParams({ package: pkg });
-  redirect(route + '?' + p.toString());
-}
+// export async function loginAction(
+//   _: LoginFormState,
+//   formData: FormData
+// ): Promise<LoginFormState> {
+//   const username = formData.get('username') + '';
+//   const password = formData.get('password') + '';
+//   const validatedFields = schema.safeParse({ username, password });
+//   if (!validatedFields.success) {
+//     return { errors: validatedFields.error.flatten().fieldErrors };
+//   }
+//   console.log('LOGIN:', username);
+//   const url = process.env.BASE_API_DOMAIN + '/login';
+//   let token, route, pkg;
+//   try {
+//     const USER = await fetch(url, {
+//       method: 'POST',
+//       credentials: 'include',
+//       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//       body: new URLSearchParams({ username, password }),
+//     }).then(async r => {
+//       if ([200, 400].includes(r.status)) return r.json();
+//       else throw new Error('Server Error');
+//     });
+//     if (!USER.token) return { errors: { message: USER.message } };
+//     cookies().set('token', USER.token, {
+//       httpOnly: true,
+//       secure: true,
+//       expires: 10700,
+//       maxAge: 10700,
+//     });
+//     token = USER.token;
+//     route = USER.menu_route;
+//     pkg = USER.package.package_name;
+//   } catch (error) {
+//     console.log(error);
+//     return { errors: { message: 'please try after sometime' } };
+//   }
+//   const p = new URLSearchParams({ package: pkg });
+//   redirect(route + '?' + p.toString());
+// }
